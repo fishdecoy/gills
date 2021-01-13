5f10bd0145c35e73dfd3727b5191886eeab35906
FishDecoy <fishdecoy1010111@gmail.com>
2021-01-13 02:23:20 -0300

    rollback - fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import GillsHeader  from "./components/header";
-import './styles/index.scss';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GillsHeader />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
